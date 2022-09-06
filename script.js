1b8e13acf9da87cd3f382e57282f49d9261e13ed
AndresFelipe <afbeltranr@unal.edu.co>
2022-09-05 23:21:55 -0500

    functions working completely
    We can encrypt, decrypt and copy the encrypted message :)

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
+const textInput = document.getElementById('textInput')
+const result = document.getElementById('result');
+
+
+
+function btnEncrypt(){
+
+    const encryptedText = encrypt(textInput.value)
+    result.value = encryptedText
+    textInput.value = ''
+
+
+}
+
+function encrypt(encryptedString){
+    
+    let codeMatrix = [['a', 'ai'],['e', 'enter'],['i', 'imes'],['o', 'ober'],['u' , 'ufat']]
+    encryptedString = encryptedString.toLowerCase();
+
+    for(let i = 0; i < codeMatrix.length; i++) {
+
+        if(encryptedString.includes(codeMatrix[i][0])){
+
+           encryptedString =  encryptedString.replaceAll(codeMatrix[i][0], codeMatrix[i][1])
+        }
+
+    return encryptedString
+    }
+}
+
+
+function btnDecrypt(){
+
+    const DecryptedText = decrypt(textInput.value)
+    result.value = DecryptedText
+    textInput.value = ''
+
+
+}
+
+function decrypt(DecryptedString){
+    
+    let codeMatrix = [['a', 'ai'],['e', 'enter'],['i', 'imes'],['o', 'ober'],['u' , 'ufat']]
+    DecryptedString = DecryptedString.toLowerCase();
+
+    for(let i = 0; i < codeMatrix.length; i++) {
+
+        if(DecryptedString.includes(codeMatrix[i][1])){
+
+           DecryptedString =  DecryptedString.replaceAll(codeMatrix[i][1], codeMatrix[i][0])
+        }
+
+    return DecryptedString
+    }
+}
+
+
+
+
+
 
 function copy(){
-    let result = document.getElementById('result');
+
     result.select()
     document.execCommand('copy'); 
     result.value = ''
